4e1224a9b055d6838514fedde43e8bee8f80a171
Den Pol <selection4test@gmail.com>
2025-01-28 13:19:17 +0300

    pwa added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import { visualizer } from 'rollup-plugin-visualizer'
 import pkg from './package.json'
 import slugify from 'slugify'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
+import { VitePWA } from 'vite-plugin-pwa'
 
+const defaultNodeEnv = 'production'
+process.env = {
+  ...process.env,
+  ...loadEnv(process.env.NODE_ENV || defaultNodeEnv, process.cwd()),
+}
+const isDev = process.env.NODE_ENV === 'development'
+const PUBLIC_URL = process.env.VITE_PUBLIC_URL || ''
 const GIT_SHA1 = process.env.VITE_GIT_SHA1
+const BRAND_NAME = process.env.VITE_BRAND_NAME || '[NN]'
+
 
 slugify.extend({ '/': '_' })
 
@@ -32,9 +42,67 @@ export default defineConfig({
   base: './',
   plugins: [
     react(),
+    VitePWA({
+      workbox: {
+        sourcemap: true,
+        maximumFileSizeToCacheInBytes: 5 * 1024 ** 2, // NOTE: 5 MB or set to something else
+      },
+      registerType: 'autoUpdate',
+      minify: false,
+      mode: isDev ? 'development' : 'production',
+      srcDir: 'public/static/pwa/', // NOTE: Default 'public'
+      outDir: 'dist',
+      filename: 'sw.js',
+      manifestFilename: 'site.webmanifest', // NOTE: Default 'manifest.webmanifest'
+      strategies: 'generateSW',
+      injectRegister: 'auto',
+      manifest: {
+        theme_color: '#ea580c',
+        background_color: '#000',
+        name: BRAND_NAME,
+        short_name: BRAND_NAME,
+        start_url: `${PUBLIC_URL}/#/?source=pwa&debug=1`,
+        // scope: PUBLIC_URL,
+        scope: './',
+        icons: [
+          {
+            src: `${PUBLIC_URL}/static/pwa/pwa-64x64.png`,
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            purpose: 'maskable',
+            sizes: '192x192',
+            src: `${PUBLIC_URL}/static/pwa/pwa-192x192.png`,
+            type: 'image/png',
+          },
+          {
+            src: `${PUBLIC_URL}/static/pwa/pwa-512x512.png`,
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            purpose: 'maskable',
+            sizes: '512x512',
+            src: `${PUBLIC_URL}/static/pwa/maskable-icon-512x512.png`,
+            type: 'image/png',
+          },
+        ],
+        orientation: 'any',
+        display: 'standalone',
+        // display_override: ["fullscreen", "minimal-ui"],
+        // dir: 'auto',
+        lang: 'ru-RU',
+      },
+      useCredentials: true,
+      includeManifestIcons: true,
+      disable: false,
+    }),
 
     // NOTE: Last one
     // See also https://www.npmjs.com/package/rollup-plugin-visualizer
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     visualizer({
       title: `Stats | Estimate Corrector v${pkg.version} | GIT SHA1 ${GIT_SHA1}`,
       template: 'sunburst', // sunburst, treemap, network
